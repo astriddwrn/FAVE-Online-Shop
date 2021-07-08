82c67a3ab872bfcbf141b8aaa55a039ef84a8c8b
Ervin Cahyadinata Sungkono <ervin.sungkono@binus.ac.id>
2021-07-08 16:16:10 +0700

    Responsive View Product DONE

---
+++ js/viewProduct.js
@@ -0,0 +1,32 @@
+var slideIndex = 1;
+
+function nextSlide(n) {
+    showSlides(slideIndex += n);
+}
+  
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+  
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName('product-img');
+    var dots = document.getElementsByClassName('dot');
+    if (n > slides.length) {slideIndex = 1}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.remove('active');
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove('active');
+    }
+    slides[slideIndex-1].classList.add('active');
+    dots[slideIndex-1].classList.add('active');
+}
+
+function validate(){
+  size = document.getElementById('size').value;
+  if(size === ""){
+    alert("Please select a size.");
+    return false;
+  }
+}
